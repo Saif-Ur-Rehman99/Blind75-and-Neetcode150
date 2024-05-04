6025ee96225fb6122f12775c4bb7df3547b20781
Saif-Ur-Rehman99 <saif.khan@xloopdigital.com>
2024-05-05 00:44:16 +0500

    problem 3

--- Medium/reversedInteger.py
+++ Medium/reversedInteger.py
@@ -8,9 +8,9 @@ def revrerseIntegrer(number: int) -> int:
     reverse = 0 
     while num > 0:
         digit = num % 10
-        if ((reverse > MAX // 10) or (reverse == MAX // 10)) and (digit >= MAX % 10):
+        if reverse > MAX // 10 or (reverse == MAX // 10 and digit > MAX % 10):
             return 0
-        if ((reverse < MIN // 10) or (reverse == MIN // 10)) and (digit <= MIN % 10):
+        if reverse < MIN // 10 or (reverse == MIN // 10 and digit < MIN % 10):
             return 0
         reverse = (reverse * 10) + digit
         num = num // 10
