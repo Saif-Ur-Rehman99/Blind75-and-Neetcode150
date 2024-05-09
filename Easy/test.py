b2825eb98ac3b1f6778dbf1f2558329aeaf5b064
Saif-Ur-Rehman99 <saif.khan@xloopdigital.com>
2024-05-09 21:38:42 +0500

    roman to integers

--- Easy/test.py
+++ Easy/test.py
@@ -1,14 +1,14 @@
 
-# def palindromeNumber(number: int) -> bool:
-#     temp = number
+def palindromeNumber(number: int) -> bool:
+    temp = number
 
-#     reverse = 0
-#     while number > 0:
-#         digit = number % 10
-#         reverse = reverse * 10 + digit
-#         number = number // 10
+    reverse = 0
+    while number > 0:
+        digit = number % 10
+        reverse = reverse * 10 + digit
+        number = number // 10
     
-#     return True if temp == reverse else False
+    return True if temp == reverse else False
 
 
-# print(palindromeNumber(13531))
+print(palindromeNumber(135531))
