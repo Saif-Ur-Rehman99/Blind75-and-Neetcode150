2e77dbade0c01145fd6fd8e8ae7d55438e871520
Saif-Ur-Rehman99 <saif.khan@xloopdigital.com>
2024-05-10 08:56:21 +0500

    problem 7: integer to roman

---
+++ Medium/integerToRoman.py
@@ -0,0 +1,29 @@
+def integerToRoman(num: int)-> str:
+    symbols = [
+        ['I', 1],
+        ['IV', 4],
+        ['V', 5],
+        ['IX', 9],
+        ['X', 10],
+        ['XL', 40],
+        ['L', 50],
+        ['XC', 90],
+        ['C', 100],
+        ['CD', 400],
+        ['D', 500],
+        ['CM', 900],
+        ['M', 1000],
+    ]
+    
+    result = ""
+    for symbol, value in reversed(symbols):
+        if num // value:
+            count = num // value
+            result += symbol * count
+            num = num % value
+    
+    return result
+
+
+
+print(integerToRoman(1000))
