24ad5c29346f0f8249f9b30de0fbcf34ec36d0b2
Saif-Ur-Rehman99 <saif.khan@xloopdigital.com>
2025-03-30 18:25:15 +0500

    66

---
+++ Easy/66_plusOne.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+def plusOne(digits: List[int]) -> List[int]:
+
+    n = len(digits) - 1
+    for i in range(n, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        digits[i] = 0
+    
+    return [1] + digits
+        
+     
+
+
+
+
+# Input: digits = [9]
+# Output: [1,0]
+# Explanation: The array represents the integer 9.
+# Incrementing by one gives 9 + 1 = 10.
+# Thus, the result should be [1,0].
+
+# Input: digits = [4,9,9,9]
+# Output: [4,3,2,2]
+
+
+# Input: digits = [4,3,2,1]
+# Output: [4,3,2,2]
+# Explanation: The array represents the integer 4321.
+# Incrementing by one gives 4321 + 1 = 4322.
+# Thus, the result should be [4,3,2,2].
+
+print(plusOne([9]))
