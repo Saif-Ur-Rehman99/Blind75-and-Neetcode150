4ce7c38c05e66b18198a70bd0a35ac66f51e3bb1
Saif-Ur-Rehman99 <saif.khan@xloopdigital.com>
2025-03-31 21:42:29 +0500

    67

---
+++ Easy/67_addBinary.py
@@ -0,0 +1,9 @@
+
+def addBinary(a: str, b: str) -> str:
+    return bin(int(a, 2) + int(b, 2))[2:]
+
+# Input: a = "11", b = "1"
+# Output: "100"
+
+# Input: a = "1010", b = "1011"
+# Output: "10101"
